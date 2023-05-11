86f3d42fdbef91b199e96dfd28989fa6f3bfadbf
Petrigh <lylabono14@gmail.com>
2023-05-11 15:10:19 -0300

    Setup project

---
+++ TP2-CDyM/main.c
@@ -0,0 +1,18 @@
+/*
+ * TP2-CDyM.c
+ *
+ * Created: 11/05/2023 15:03:38
+ * Author : lylab
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
