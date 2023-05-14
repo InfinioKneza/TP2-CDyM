e20d09d4b92514310739a05ee28aa393d64bb1dd
InfinioKneza <luciocanessa@gmail.com>
2023-05-14 16:55:23 -0300

    Añadido la hora actual inicial.

--- TP2-CDyM/main.c
+++ TP2-CDyM/main.c
@@ -10,13 +10,28 @@
 #include "lcd.h"
 #include "timer.h"
 #include "teclado.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "stdint.h"
+
+uint8_t hora,minuto, segundo;
+
+
+void obtener_hora_actual() {
+	const char* compilationTime = __TIME__;  // Store the compilation time in a variable
+	
+	hora = ((atoi(&compilationTime[0]))*10 + (atoi(&compilationTime[1]))) / 10;
+	minuto = ((atoi(&compilationTime[3]))*10 + (atoi(&compilationTime[4]))) /10;
+	segundo = ((atoi(&compilationTime[6]))*10 + (atoi(&compilationTime[7]))) /10;
+}
 
 int main(void)
 {
 	// Inicializar LCD
 	LCD_Init();
 	// inicializar clock
-	CLOCK_Init(6,58,30);
+	obtener_hora_actual();
+	CLOCK_Init(hora,minuto,segundo);
 	// inicializar teclado
 	TECLADO_Init();
 	// inicializar maq de estados y buffers
