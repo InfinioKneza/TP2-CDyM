03646cc00c573bf8264ce5f14d36b0f76a6600c4
Petrigh <lylabono14@gmail.com>
2023-05-22 20:01:39 -0300

    calibracion timer0

--- TP2-CDyM/timer.c
+++ TP2-CDyM/timer.c
@@ -13,6 +13,7 @@ static uint8_t clock_MEF = 0;
 //Activación de interrupción periódica y
 //Generación de señal periódica en terminal OC0A (PD6) 
 void Timer0Init(void){
+	DDRC |= (1<<DDC4) | (1<<DDC5);
 	//configuración del TOPE del contador TCNT0 
 	OCR0A=249;			//249+1
 	TCCR0A =(1<<COM0A0) | (1<<WGM01); //modo CTC, Toggle on compare match
@@ -24,12 +25,13 @@ void Timer0Init(void){
 //se activa periodicamente cuando TCNT0==OCR0A modo CTC
 ISR(TIMER0_COMPA_vect){ //interrupción periódica de periodo Tisr=250/250KHz=1ms  o fisr=250KHz/250=1KHz
 	if(++cont_MEF == 10 ){ 
-		if(++clock_MEF==100){
+		if(++clock_MEF==89){ //Actualiza en la iteracion 890 por retardos del programa
 			CLOCK_Update();
 			clock_MEF=0;
 			PORTC ^= (1<<PORTC4);
 		}
 		cont_MEF=0;
 		CERRADURA_Update();
+		PORTC ^= (1<<PORTC5);
 	}
 }
